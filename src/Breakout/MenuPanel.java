a89cc7cbc0c74551e189e1d303bb1c2e947d24b1
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-22 04:24:22 +0530

    Main Menu and Levels Panel

--- src/Breakout/MenuPanel.java
+++ src/Breakout/MenuPanel.java
@@ -10,61 +10,64 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionEvent;
 
 public class MenuPanel extends JPanel {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	public JButton easyButton;
-	public JButton mediumButton;
-	public JButton hardButton;
+	private String username;
 	private JButton logoutButton;
+	private JButton btnLeaderboard;
+	private JButton btnPlayGame;
+    private JLabel lblNewLabel;
     public MenuPanel() {
         setBackground(Color.BLACK);
         setForeground(Color.WHITE);
         setLayout(null);
         
-        easyButton = new JButton("Easy");
-        easyButton.setFont(new Font("Impact", Font.PLAIN, 24));
-        easyButton.setActionCommand("Easy");
-        easyButton.setBounds(329, 299, 145, 43);
-        easyButton.setBackground(Color.GREEN);
-        add(easyButton);
-        
-        mediumButton = new JButton("Medium");
-        mediumButton.setFont(new Font("Impact", Font.PLAIN, 24));
-        mediumButton.setActionCommand("Medium");
-        mediumButton.setBackground(Color.ORANGE);
-        mediumButton.setBounds(329, 365, 145, 43);
-        add(mediumButton);
-        
-        hardButton = new JButton("Hard");
-        hardButton.setFont(new Font("Impact", Font.PLAIN, 24));
-        hardButton.setActionCommand("Hard");
-        hardButton.setForeground(Color.BLACK);
-        hardButton.setBackground(Color.RED);
-        hardButton.setBounds(329, 427, 145, 43);
-        add(hardButton);
-        
-        JLabel levelLabel = new JLabel("Choose Level");
+        JLabel levelLabel = new JLabel("Breakout");
         levelLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        levelLabel.setFont(new Font("Impact", Font.PLAIN, 45));
-        levelLabel.setForeground(Color.WHITE);
-        levelLabel.setBounds(262, 175, 274, 82);
+        levelLabel.setFont(new Font("Impact", Font.PLAIN, 50));
+        levelLabel.setForeground(new Color(255, 153, 255));
+        levelLabel.setBounds(266, 83, 274, 82);
         add(levelLabel);
         
         logoutButton = new JButton("Logout");
         logoutButton.setToolTipText("");
-        logoutButton.setForeground(Color.DARK_GRAY);
+        logoutButton.setForeground(new Color(255, 255, 204));
         logoutButton.setFont(new Font("Impact", Font.PLAIN, 20));
         logoutButton.setBackground(Color.RED);
-        logoutButton.setBounds(614, 91, 127, 43);
+        logoutButton.setBounds(300, 489, 209, 58);
         add(logoutButton);
+        
+        lblNewLabel = new JLabel("Welcome " + username);
+        lblNewLabel.setFont(new Font("Impact", Font.PLAIN, 35));
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setForeground(new Color(153, 255, 255));
+        lblNewLabel.setBounds(206, 207, 389, 58);
+        add(lblNewLabel);
+        
+        btnLeaderboard = new JButton("Leaderboard");
+        btnLeaderboard.setForeground(new Color(255, 255, 255));
+        btnLeaderboard.setBackground(new Color(51, 153, 255));
+        btnLeaderboard.setFont(new Font("Impact", Font.PLAIN, 20));
+        btnLeaderboard.setBounds(300, 404, 209, 58);
+        add(btnLeaderboard);
+        
+        btnPlayGame = new JButton("Play Game");
+        btnPlayGame.setForeground(new Color(255, 255, 255));
+        btnPlayGame.setBackground(new Color(0, 204, 0));
+        btnPlayGame.setFont(new Font("Impact", Font.PLAIN, 20));
+        btnPlayGame.setBounds(300, 323, 209, 58);
+        add(btnPlayGame);
     }
-
-    public void setStartGameAction(ActionListener actionListener) {
-        easyButton.addActionListener(actionListener);
-        mediumButton.addActionListener(actionListener);
-        hardButton.addActionListener(actionListener);
+    public void setUsername(String username) {
+        System.out.println("Current Player: " + username + " Received.");
+    	this.username = username;
+        lblNewLabel.setText("Welcome " + username);
+    }
+    public void setPlayGameAction(ActionListener actionListener) {
+    	btnPlayGame.addActionListener(actionListener);
+    }
+    
+    public void setLeaderboardAction(ActionListener actionListener) {
+    	btnLeaderboard.addActionListener(actionListener);
     }
 
     public void setLogoutAction(ActionListener actionListener) {
