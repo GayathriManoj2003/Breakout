a94bb601f65bd636a2eedd7e9e4c07825f3d9a9a
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-22 04:25:40 +0530

    Add Login Form with Verification

--- src/Breakout/Login.java
+++ src/Breakout/Login.java
@@ -3,21 +3,27 @@ package Breakout;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
 
 public class Login extends JPanel {
 	private JTextField textField;
-	private JTextField textField_1;
+	private JPasswordField passwordField;
 	public JButton btnNewButton;
 	public JButton btnSignUp;
+	public JLabel messageLabel;
 	public Login() {
-        setBackground(Color.DARK_GRAY);
+        setBackground(new Color(0, 0, 51));
         setLayout(null);
         
         JLabel lblNewLabel = new JLabel("PLAYER LOGIN");
@@ -39,11 +45,10 @@ public class Login extends JPanel {
         add(textField);
         textField.setColumns(10);
         
-        textField_1 = new JTextField();
-        textField_1.setFont(new Font("Wingdings 2", Font.PLAIN, 10));
-        textField_1.setColumns(10);
-        textField_1.setBounds(354, 312, 220, 39);
-        add(textField_1);
+        passwordField = new JPasswordField();
+        passwordField.setColumns(10);
+        passwordField.setBounds(354, 312, 220, 39);
+        add(passwordField);
         
         JLabel lblNewLabel_1_1 = new JLabel("Password:");
         lblNewLabel_1_1.setForeground(Color.CYAN);
@@ -59,15 +64,21 @@ public class Login extends JPanel {
         add(btnNewButton);
         
         btnSignUp = new JButton("Sign Up");
+        btnSignUp.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
         btnSignUp.setFont(new Font("Impact", Font.PLAIN, 20));
         btnSignUp.setBackground(Color.YELLOW);
         btnSignUp.setBounds(316, 486, 146, 45);
         add(btnSignUp);
         
-        JLabel lblNewLabel_2 = new JLabel("");
-        lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        lblNewLabel_2.setBounds(211, 579, 375, 54);
-        add(lblNewLabel_2);
+        messageLabel = new JLabel("");
+        messageLabel.setForeground(Color.RED);
+        messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        messageLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        messageLabel.setBounds(189, 579, 414, 54);
+        add(messageLabel);
     }
 	public void setSignUpAction(ActionListener actionListener) {
 		btnSignUp.addActionListener(actionListener);
@@ -75,8 +86,40 @@ public class Login extends JPanel {
 	public void setLoginAction(ActionListener actionListener) {
 		btnNewButton.addActionListener(actionListener);
     }
+	public Player verifyPlayer() {
+		messageLabel.setText("");
+		
+		String username = textField.getText();
+        String password = new String(passwordField.getPassword());
+        
+		try {
+			String query = "SELECT * FROM Player WHERE username = ? AND password = ? ";
+            Connection connection = ConnectionManager.getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs.next()) {
+                // Player exists with the given user name and password
+            	this.reset();
+                return new Player(username);
+            } else {
+                // No player found with the given credentials
+                this.reset();
+                messageLabel.setText("Incorrect Username or Password.");
+                return null;
+            }
+   
+		}	catch ( Exception e ) {
+			System.out.println("Error: " + e);
+			messageLabel.setText("Error: Failed to Login");
+		}
+		return null;
+	}
 	public void reset() {
 		textField.setText("");
-		textField_1.setText("");
+		passwordField.setText("");
 	}
 }
