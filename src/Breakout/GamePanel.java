cade89b32ccdcd9177794a1cd527686b7f810899
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-22 19:17:26 +0530

    Change Font

--- src/Breakout/GamePanel.java
+++ src/Breakout/GamePanel.java
@@ -69,10 +69,13 @@ public abstract class GamePanel extends JPanel {
     private void updateGameState() {
         if (currentState instanceof PlayingState) {
             currentState.setMessage("Game is playing. Press 'P' to pause or 'Q' to quit.");
+            timer.start();
         } else if (currentState instanceof PausedState) {
             currentState.setMessage("Game is paused. Press ENTER to resume.");
+            timer.stop();
         } else if (currentState instanceof QuitState) {
             currentState.setMessage("Quit game");
+            timer.stop();
         }
     }
     
@@ -89,23 +92,28 @@ public abstract class GamePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        var g2d = (Graphics2D) g;
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
         // Playing paint
         if( currentState instanceof PlayingState ) {
-        	var g2d = (Graphics2D) g;
-
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                    RenderingHints.VALUE_ANTIALIAS_ON);
-
-            g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
-                    RenderingHints.VALUE_RENDER_QUALITY);
+        	
 
             playingState.drawObjects(g2d);
-
-            Toolkit.getDefaultToolkit().sync();
-        	System.out.println( "Playing State");
+//        	System.out.println( "Playing State");
         } else {
-            g.drawString(currentState.getMessage(), 50, 50);
-            System.out.println( "Message is: "+ currentState.getMessage());
+        	playingState.drawObjects(g2d);
+        	Font font = new Font("Impact", Font.PLAIN, 25);
+            g.setFont(font);
+        	g.setColor(Color.BLACK);
+            g.drawString(currentState.getMessage(), 200, 320);
+//            System.out.println( "Message is: "+ currentState.getMessage());
         }
+
+        Toolkit.getDefaultToolkit().sync();
     }
 }
