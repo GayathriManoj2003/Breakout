76416d2aac611de8fe06696c5aaf6e1034ddaaba
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-22 23:34:10 +0530

    Add Bricks

---
+++ src/Breakout/BrickController.java
@@ -0,0 +1,34 @@
+// BrickController.java
+package Breakout;
+
+import java.awt.*;
+
+public class BrickController {
+    private Brick[][] bricks;
+
+    public BrickController() {
+    	
+    }
+    public BrickController( int width, int height) {
+        int rows = height/20;
+        int cols = width/80;
+        bricks = new Brick[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int x = col * 80 + 10;
+                int y = row * 20 + 10;
+                Color color = Color.YELLOW;
+                bricks[row][col] = new Brick(x, y, 80, 20, color);
+            }
+        }
+    }
+
+    public void drawBricks(Graphics2D g) {
+        for (int row = 0; row < bricks.length; row++) {
+            for (int col = 0; col < bricks[row].length; col++) {
+                bricks[row][col].draw(g);
+            }
+        }
+    }
+}
