d551f8b3434da0c2645381fea583b0d4f5fdbf7c
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-22 19:02:01 +0530

    Define Template Functions for Playing State

--- src/Breakout/PlayingState.java
+++ src/Breakout/PlayingState.java
@@ -1,16 +1,56 @@
 package Breakout;
 
-import javax.swing.*;
-import java.awt.event.KeyEvent;
+import java.awt.Graphics2D;
 
 class PlayingState implements GameState {
-//    @Override
-//    public void handleKey(GameFrame frame, KeyEvent e) {
-//        int keyCode = e.getKeyCode();
-//        if (keyCode == KeyEvent.VK_P) {
-//            frame.setCurrentState(new PausedState());
-//        } else if (keyCode == KeyEvent.VK_Q) {
-//            frame.setCurrentState(new QuitState());
-//        }
-//    }
+	private String message;
+	private BallController ballController;
+	
+	// Create objects of Controllers within constructor of Playing state.
+	// Also create collision handler object
+	
+	PlayingState() {
+		this.ballController = new BallController( 0, 0, 10, 10);
+	}	
+	
+	PlayingState( int ballX, int ballY, int ballVelX, int ballVelY) {
+		this.ballController = new BallController(ballX, ballY, ballVelX, ballVelY);
+	}	
+	
+	public void setBallVelocity( int velX, int velY) {
+		ballController.setVelX(velX);
+		ballController.setVelY(velY);
+	}
+	
+	public void timeStep() {
+		
+		ballController.moveBall();
+		
+		// Move Paddle Here
+		
+		// Check Ball Boundaries within CollionHandler
+		// Detect Collision through CollionHandler over Here
+		
+	}
+
+	public void drawObjects(Graphics2D g2d) {
+		
+		// Draw Other objects Also by calling functions
+		// Can Split into MVC by placing these functions inside View Classes for 
+		// each object and using the View Class inside Controller of Object
+		
+		ballController.drawBall(g2d);
+    }
+	
+	@Override
+	public void setMessage(String message) {
+		// TODO Auto-generated method stub
+		this.message = "" ;
+	}
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return this.message;
+	}
 }
