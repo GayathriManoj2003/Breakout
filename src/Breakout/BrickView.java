6dcde37fa350603569307d809326a15e00c4789d
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-23 22:48:27 +0530

    Colors for Durability

--- src/Breakout/BrickView.java
+++ src/Breakout/BrickView.java
@@ -2,11 +2,19 @@
 package Breakout;
 
 import java.awt.*;
+import java.util.HashMap;
 
 public class BrickView {
-
-    public void drawBrick(Graphics2D g2d, int x, int y, int width, int height, Color color) {
-        g2d.setColor(color);
+	HashMap<Integer, Color> mp;
+	BrickView() {
+		this.mp = new HashMap<Integer, Color>();
+		mp.put(1, Color.YELLOW);
+		mp.put(2, Color.ORANGE);
+		mp.put(3, Color.RED);
+	}
+    public void drawBrick(Graphics2D g2d, int x, int y, int width, int height, int durability) {
+        g2d.setColor(mp.get(durability));
+        
         g2d.fillRect(x, y, width, height);
         g2d.setColor(Color.BLACK);
         g2d.setStroke(new BasicStroke(3));
