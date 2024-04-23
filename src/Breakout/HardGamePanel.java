5244db2ed70144658756a476d5829549698cd4a8
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-23 10:53:00 +0530

    Pass Game Level Specifics

--- src/Breakout/HardGamePanel.java
+++ src/Breakout/HardGamePanel.java
@@ -7,8 +7,11 @@ public class HardGamePanel extends GamePanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static int numberOfBrickRows = 12;
+	private static int ballVelocityX = 10;
+	private static int ballVelocityY = 10;
 
 	public HardGamePanel() {
-        setBackground(Color.RED);
+		super( ballVelocityX, ballVelocityY, numberOfBrickRows, Color.BLUE, Color.RED);
     }
 }
